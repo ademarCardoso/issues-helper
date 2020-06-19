7374eaa7975deba00871e046d442aa4c6919892b
ademarCardoso <ac@storyblok.com>
2020-06-19 00:51:28 -0300

    add logo

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <img alt="Stroyblok logo" src="./assets/logo.png">
     <ListOfRepositories />
   </div>
 </template>
@@ -17,12 +17,9 @@ export default {
 </script>
 
 <style>
-/* #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+#app img {
+  margin-top: 20px;
+  width: 50%;
+  justify-content: center;
+}
 </style>
